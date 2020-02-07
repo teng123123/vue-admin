4d733ba4c7d666f9bbcd9052c0db8b2b864a9a80
滕庆 <2012332380@qq.com>
2020-02-07 11:34:36 +0800

    构建项目，全局样式，路由

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+// import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    redirect: "login"
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component:()=> import("../views/Login/index.vue")
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
